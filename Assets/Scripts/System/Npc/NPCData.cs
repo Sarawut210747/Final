50da78c3d5c7c8ac8ba0a15e125b8902e5ed1f60
Sarawut210747 <sarawut.wongsr@bumail.net>
2025-10-07 15:16:08 +0700

    แก้

--- Assets/Scripts/System/Npc/NPCData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class NPCData
-{
-    public string npcName = "NPC";
-    public int happiness = 50;
-    public string preferredRoomType = "Normal";
-}
